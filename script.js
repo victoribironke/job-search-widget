87a41bec296d6a170758f4b676b380d488cde091
Victor Ibironke <victoribironke2019@gmail.com>
2024-04-16 18:42:24 +0100

    modified script js

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const fetchData = () => {
+document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("widget-container");
 
   const search = container.dataset.search;
 
   const query = search ? search : "snickare";
+
+  const populateTable = (data) => {
+    // console.log(data);
+    const tableBody = document.getElementById("table-body");
+
+    tableBody.innerHTML = "";
+
+    data.forEach((item) => {
+      const row = document.createElement("tr");
+
+      if (data.indexOf(item) % 2 !== 0) {
+        row.style.backgroundColor = "#f2f2f2";
+      }
+
+      row.innerHTML = `
+        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;"><a>${item.headline}</a></td>
+        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">${item.duration.label}</td>
+        <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">${item.number_of_vacancies}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+
+    const anchors = Array.from(tableBody.getElementsByTagName("a"));
+
+    anchors.forEach((i) => {
+      i.href = data[anchors.indexOf(i)].webpage_url;
+    });
+  };
 
   fetch(
     "https://jobsearch.api.jobtechdev.se/search?q=" +
@@ -22,34 +50,4 @@ const fetchData = () => {
       // console.error(e);
       alert("An error occured while fetching data.");
     });
-};
-
-const populateTable = (data) => {
-  // console.log(data);
-  const tableBody = document.getElementById("table-body");
-
-  tableBody.innerHTML = "";
-
-  data.forEach((item) => {
-    const row = document.createElement("tr");
-
-    if (data.indexOf(item) % 2 !== 0) {
-      row.style.backgroundColor = "#f2f2f2";
-    }
-
-    row.innerHTML = `
-      <td style="border: 1px solid #ddd; padding: 8px; text-align: left;"><a>${item.headline}</a></td>
-      <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">${item.duration.label}</td>
-      <td style="border: 1px solid #ddd; padding: 8px; text-align: left;">${item.number_of_vacancies}</td>
-    `;
-    tableBody.appendChild(row);
-  });
-
-  const anchors = Array.from(tableBody.getElementsByTagName("a"));
-
-  anchors.forEach((i) => {
-    i.href = data[anchors.indexOf(i)].webpage_url;
-  });
-};
-
-document.addEventListener("DOMContentLoaded", fetchData);
+});
